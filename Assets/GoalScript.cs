55c0ee15b07d6ba5c341678982f3e13f5ce1409b
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2019-04-27 11:01:19 +0200

    Add: Goalscript

---
+++ Assets/GoalScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    public GameController Controller;
+    public Vector3 HatPosition;
+    
+    void OnTriggerEnter(Collider triggerCollider)
+    {
+        var go = triggerCollider.gameObject;
+
+        if (go.GetComponent<PlayerScript>() != null)
+        {
+            Controller.TriggerWin();
+
+            var rb = go.GetComponent<Rigidbody>();
+            rb.isKinematic = true;
+
+            go.transform.position = transform.position + HatPosition;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + HatPosition, 0.1f);
+    }
+}
