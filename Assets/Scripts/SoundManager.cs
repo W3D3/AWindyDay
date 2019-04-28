8e78f1a69ca6b4436db253e503f68c6d0e4a880b
Monomus <christof.bachmann@gmail.com>
2019-04-28 10:06:57 +0200

    Soundmanager added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource wind;
+    public AudioClip windClip;
+    public AudioSource win;
+    public AudioClip winClip;
+    public AudioSource defeat;
+    public AudioClip defeatClip;
+
+    public static SoundManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = null;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public void playWind()
+    {
+        wind.clip = windClip;
+        wind.Play();
+    }
+
+    public void playWin()
+    {
+        win.clip = winClip;
+    }
+}
